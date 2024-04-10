d0b241562bd3229a4c6b833ec8e4ef0985ce2799
fox <102655383+f0xb17@users.noreply.github.com>
2024-04-10 16:00:04 +0200

    Update install.sh
    - Added Logitech Software
    - Added KDE Shell Script to implement Triple Buffer
    - Added xorg.conf to implement Triple Buffer

--- install.sh
+++ install.sh
@@ -49,6 +49,16 @@ if [[ "$1" == "--kde" ]]; then
 
     sudo pacman -S --needed --noconfirm flatpak xdg-desktop-portal xdg-desktop-portal-kde xdg-desktop-portal-gtk okular spectacle kcalc
     
+    cp -r files/kwin.sh $HOME/.dotconf
+    chmod a+x $HOME/.dotconf/kwin.sh
+    cp -r files/xorg.conf $HOME/.dotconf
+    
+    mkdir $HOME/.config/plasma-workspace
+    mkdir $HOME/.config/plasma-workspace/env
+    
+    ln -sf $HOME/.dotconf/kwin.sh $HOME/.config/plasma-workspace/env/
+    sudo ln -sf $HOME/.dotconf/xorg.conf /etc/X11/
+    
     echo "---------------------------------------------------------------"
 fi
 
@@ -119,7 +129,6 @@ packages=(
     "earlyoom"
     "parted"
     "highlight"
-    
 )
 
 for package in "${packages[@]}"; do
@@ -248,6 +257,7 @@ apps=(
     "gwenview"
     "vlc"
     "obs-studio"
+    "solaar"
 )
 
 sdks=(
