a529ec93aac8fbbaebdbe43550fd6d7087755015
fox <102655383+f0xb17@users.noreply.github.com>
2024-11-22 15:48:53 +0100

    Update .wezterm.lua
    Added maximized start

--- .config/wezterm/.wezterm.lua
+++ .config/wezterm/.wezterm.lua
@@ -1,8 +1,9 @@
 -- Wezterm API
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
 
 -- Getting the Configuration API
-local config = wezterm.config_builder()
+local config = {}
 
 -- Theme Configuration
 config.color_scheme = 'Catppuccin Mocha (Gogh)'
@@ -14,9 +15,15 @@ config.line_height = 1.5
 
 -- Window Configuration
 config.enable_tab_bar = false
-config.initial_cols = 150
-config.initial_rows = 50
+-- config.initial_cols = 150
+-- config.initial_rows = 50
 config.window_background_opacity = 0.9
 config.window_decorations = "NONE"
+
+-- Start window maximized
+wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
+end)
 
 return config
