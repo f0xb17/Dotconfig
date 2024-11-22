0019cfad3c7bd2fe24e1408e07618e4389a0b7bf
fox <102655383+f0xb17@users.noreply.github.com>
2024-11-22 13:20:19 +0100

    Update .wezterm.lua
    Changed some configuration

--- .config/wezterm/.wezterm.lua
+++ .config/wezterm/.wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Theme Configuration
-config.color_scheme = 'catppuccin-mocha'
+config.color_scheme = 'Catppuccin Mocha (Gogh)'
 
 -- Font Configuration
 config.font = wezterm.font("FiraCode Nerd Font Mono")
@@ -15,6 +15,7 @@ config.font_size = 13
 config.enable_tab_bar = false
 config.initial_cols = 150
 config.initial_rows = 50
--- config.window_background_opacity = 0.9
+config.window_background_opacity = 0.9
+config.window_decorations = "NONE"
 
 return config
